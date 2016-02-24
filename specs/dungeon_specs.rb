8e83a936f8406b2d2fb9ac3960e7ee9133e82ea4
Francesco <fpfrancescopalma7@gmail.com>
2016-02-24 13:41:30 +0000

    Adds modifiers, tests pass

--- specs/dungeon_specs.rb
+++ specs/dungeon_specs.rb
@@ -7,12 +7,25 @@ class TestDungeon< MiniTest::Test
 
   def setup
 
-    @dungeon = Dungeon.new(5)
+    modifiers = {
+      2 => @monster,
+      4 => @enigma
+    }
+
+    @dungeon = Dungeon.new(5, modifiers)
 
   end
 
   def test_board_should_have_5_tiles
-    assert_equal(5, @dungeon.path.size )
+    assert_equal(5, @dungeon.path.size)
+  end
+
+  def test_modifier_2
+    assert_equal(@monster, @dungeon.path[2])
+  end
+
+  def test_modifier_4
+    assert_equal(@enigma, @dungeon.path[4])
   end
 
   def test_end_tile
