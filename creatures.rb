dee45d8ca0b8788493c75a38c863e9be2178829a
rpmonteiro <ric.p.monteiro@gmail.com>
2016-02-24 12:09:00 +0000

    attack functionality added with tests passing

--- creatures.rb
+++ creatures.rb
@@ -1,22 +1,34 @@
 class Creatures
 
-  attr_reader :name, :health, :attack  
+  attr_reader :name, :health, :attack_damage  
 
   def initialize(params)
     @name = params[:name]
     @health = params[:health]
-    @attack = params[:attack]
+    @attack_damage = params[:attack_damage]
+  end
+
+  def attack( target )
+    target.health - @attack_damage
   end
 
 end
 
 
 class Player < Creatures
-  attr_reader :name, :health, :attack
+  attr_reader :name, :health, :attack_damage
+
+  def move()
+
+  end
+
+  def rest()
+
+  end
 
 end
 
 class Monster < Creatures
-  attr_reader :name, :health, :attack
+  attr_reader :name, :health, :attack_damage
 
 end
--- specs/creatures_specs.rb
+++ specs/creatures_specs.rb
@@ -6,15 +6,15 @@ require_relative '../creatures'
 class TestCreatures < Minitest::Test
 
   def setup
-   @creatures1 = Creatures.new(name: "Topogigio", health: 75, attack: 10)
+   @creatures1 = Creatures.new(name: "Topogigio", health: 75, attack_damage: 10)
 
-   @player = Player.new(name: "Spanky", health: 100, attack: 20 )
+   @player = Player.new(name: "Spanky", health: 100, attack_damage: 20 )
 
-   @monster1 = Monster.new(name: "Dragon", health: 75, attack: 10)
+   @monster1 = Monster.new(name: "Dragon", health: 75, attack_damage: 10)
   end
 
-  
-#### Creatures
+
+  #### Creatures Object
   def test_creature_has_name
     assert_equal("Topogigio", @creatures1.name)
   end
@@ -24,11 +24,11 @@ class TestCreatures < Minitest::Test
   end
 
   def test_creature_has_attack
-    assert_equal(10, @creatures1.attack)
+    assert_equal(10, @creatures1.attack_damage)
   end
 
 
-#### Player
+  #### Player Object
   def test_player_has_name
     assert_equal("Spanky", @player.name)
   end
@@ -38,10 +38,10 @@ class TestCreatures < Minitest::Test
   end
 
   def test_player_has_attack
-    assert_equal(20, @player.attack)
+    assert_equal(20, @player.attack_damage)
   end
 
-  #### Monster
+  #### Monster Object
   def test_monster_has_name
     assert_equal("Dragon", @monster1.name)
   end
@@ -51,7 +51,15 @@ class TestCreatures < Minitest::Test
   end
 
   def test_monster_has_attack
-    assert_equal(10, @monster1.attack)
+    assert_equal(10, @monster1.attack_damage)
   end
+
+  ### Player attack
+  def test_player_attacks
+    expected_result = @monster1.health - 20
+    @player.attack( @monster1 )
+    assert_equal( expected_result, @player.attack(@monster1)) 
+  end
+
 
 end
